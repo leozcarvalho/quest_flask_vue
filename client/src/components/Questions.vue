61c4601838ee283db495cb8110571c6bb173e258
leozcarvalho <leo.zcarvalho@hotmail.com>
2021-07-15 10:07:51 -0300

    Front pronto

--- client/src/components/Questions.vue
+++ client/src/components/Questions.vue
@@ -1,33 +1,43 @@
 <template>
 <div class="container">
-    <h1>Interrogatório Criminal</h1>
     <div v-if="this.questStart===false" class="container my-5">
-        <h2>Um crime foi cometido responda o questionário
+        <h1>Interrogatório Criminal</h1>
+        <h2 class="mt-5">Um crime foi cometido. Insira seu nome e responda o questionário
         para colaborar com a investigação.</h2>
         <button
         type="button"
-        class="btn btn-success mt-4"
+        class="btn btn-success mt-4 btn-lg"
         v-on:click.stop.prevent = startQuest
         >Iniciar</button>
     </div>
-    <div v-else class="container my-5">
+    <transition name="fade">
+    <div v-if="this.questStart && this.questFinish == false"
+    class="container my-5">
         <div class="row">
             <div class="col"></div>
-            <div class="col-8 text-center">
-                <h2>{{questions[index]}}</h2>
-                <button type="button"
-                class="btn btn-success
-                float-right mt-5"
-                v-on:click.stop.prevent = getAnswerYes
-                >Sim</button>
-                <button type="button"
-                class="btn btn-danger
-                float-right mt-5"
-                v-on:click.stop.prevent = getAnswerNo
-                >Não</button>
+            <div class="col-8">
+                <h1>Questão {{this.index + 1}}</h1>
+                <h2 class="text-center mt-5">{{questions[index]}}</h2>
+                <div class="float-right">
+                    <button type="button"
+                    class="btn btn-success
+                    mt-5 mr-3 btn-lg"
+                    v-on:click.stop.prevent = getAnswerYes
+                    >Sim</button>
+                    <button type="button"
+                    class="btn btn-danger
+                    mt-5 mr-2 btn-lg"
+                    v-on:click.stop.prevent = getAnswerNo
+                    >Não</button>
+                </div>
             </div>
             <div class="col"></div>
         </div>
+    </div>
+    </transition>
+    <div v-if="this.questFinish" class="container mt-5 text-center">
+        <h1>Resultado</h1>
+        <h2 class="mt-5">{{result}}</h2>
     </div>
 </div>
 </template>
@@ -38,6 +48,7 @@ export default {
   data() {
     return {
       questStart: false,
+      questFinish: false,
       questions: ['Telefonou para a vítima?',
         'Esteve no local do Crime?',
         'Mora perto da vítima?',
@@ -45,6 +56,7 @@ export default {
         'Já trabalhou com a vítima?'],
       index: 0,
       answers: [],
+      result: '',
     };
   },
 
@@ -54,6 +66,9 @@ export default {
     },
     nextQuest() {
       this.index += 1;
+      if (this.index === 5) {
+        this.questFinished();
+      }
     },
     getAnswerYes() {
       this.answers.push('sim');
@@ -65,6 +80,39 @@ export default {
       console.log(this.answers);
       this.nextQuest();
     },
+    questFinished() {
+      this.questFinish = true;
+      this.questResult();
+    },
+    questResult() {
+      let answers = 0;
+      let aux = this.answers.indexOf('sim');
+      while (aux !== -1) {
+        answers += 1;
+        aux = this.answers.indexOf('sim', aux + 1);
+      }
+      if (answers === 2) {
+        this.result = 'Suspeito';
+      }
+      if (answers >= 3 || answers <= 4) {
+        this.result = 'Cúmplice';
+      }
+      if (answers === 5) {
+        this.result = 'Assassino';
+      }
+      if (answers === 0) {
+        this.result = 'Inocente';
+      }
+    },
   },
 };
 </script>
+
+<style>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+</style>
--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import 'bootstrap/dist/css/bootstrap.css';
+import BootstrapVue from '../node_modules/bootstrap-vue';
+
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
 
